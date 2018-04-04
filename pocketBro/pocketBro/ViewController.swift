722e611e068d3b24c868de7eacd3992c3b770b9b
vasilkovskii <36913298+vasilkovskii@users.noreply.github.com>
2018-04-04 11:30:16 +0300

    гг
    основа Максима с линей таблиц для факультета иу

---
+++ pocketBro/pocketBro/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  pocketBro
+//
+
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   // UITableViewDataSource, UITableViewDelegate {
+    //private let dwarves = [
+    //"Иу","Мт","Рл","Фн","См","Э","Рк","Бмт","Юр","Ибм","Л","Сгн","Ркт","Ак","Пс"]
+   // let simpleTableIdentifier = "SimpleTableIdentifier"
+   // }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+   // func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+   //     return dwarves.count
+   // }
+   // func tableView(_ tableView:UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+      //  var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifer)
+      //  if (cell==nil)
+      // {
+           // cell = UITableViewCell(style: UITableViewStyle.default, reuseIdentifier: simpleTableIdentifier)
+       // }
+       // cell?.textLabel?.text = dwarves[indexPath.row]
+       // return cell!!
+    }
+    
+
+
