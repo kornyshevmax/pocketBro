0d7b41c1fa29452495526715574c2443730802c0
vasilkovskii <36913298+vasilkovskii@users.noreply.github.com>
2018-04-06 13:23:17 +0300

    Changes
    Добавил TableView  , и примерный вид библиотеки.

---
+++ pocketBro/pocketBro/TableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TableViewController.swift
+//  pocketBro
+//
+//  Created by ольга васильковская on 06.04.2018.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
